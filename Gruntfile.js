54072ce6561591201ba23641f3dc87592bc6a7d1
Ruslan Dulina <Ruslan_Dulina@epam.com>
2014-08-28 14:56:24 +0300

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function (grunt) {
+    'use strict';
+    grunt.initConfig({
+        bower: {
+            install: {
+                options: {
+                    targetDir: 'node_modules/appsngen-dev-box/js/dependencies',
+                    layout: 'byComponent',
+                    install: true,
+                    verbose: false,
+                    cleanTargetDir: false,
+                    cleanBowerDir: true,
+                    bowerOptions: {}
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-bower-task');
+};
---
+++ startDevBox.js
@@ -0,0 +1,5 @@
+(function () {
+    'use strict';
+	require('./node_modules/appsngen-viewer/server');
+	require('./node_modules/appsngen-dev-box/server');
+}());
